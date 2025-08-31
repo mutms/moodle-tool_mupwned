cf3c2a7dd5742650e9ae6dfd8df692e60e79c107
Petr Skoda <petr@mutms.org>
2025-08-31 10:41:56 +0200

    Prepare mu-4.5.6-02 release

--- CHANGES.md
+++ CHANGES.md
@@ -2,7 +2,7 @@
 
 ## mu-4.5.6-02
 
-Release date: xx/08/2025
+Release date: 31/08/2025
 
 * Fixed debugging when not logged in as admin.
 
--- version.php
+++ version.php
@@ -28,9 +28,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = 2025081845;
+$plugin->version = 2025083145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.6-01+';
+$plugin->release = 'mu-4.5.6-02';
