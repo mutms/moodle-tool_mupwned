78b28a16356f652827ca14b4f3b79001d2143916
Petr Skoda <petr@mutms.org>
2025-09-25 13:31:35 +0200

    Bump release

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->version = 2025092450;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [5000, 5000];
-$plugin->release = 'mu-5.0.2-03';
+$plugin->release = 'mu-5.0.2-03+';
