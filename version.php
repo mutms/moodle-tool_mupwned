d40a669db0443a917af0be4fcf1e84c8a2f4f3f2
Petr Skoda <petr@mutms.org>
2025-12-07 11:05:49 +0100

    Prepare 4.5.8 release

--- version.php
+++ version.php
@@ -28,9 +28,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = 2025120345;
+$plugin->version = 2025120845;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.7-02+';
+$plugin->release = 'mu-4.5.8-01';
