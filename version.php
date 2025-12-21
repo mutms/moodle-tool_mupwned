5966d94d9591a1adba12da2d54812add24de0a9f
Petr Skoda <petr@mutms.org>
2025-12-21 17:43:10 +0100

    Bump version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = 2025121645;
+$plugin->version = 2025122145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
