9c4025c4bd5fdf8bfbb8a6dafa552fdaabe32384
Petr Skoda <petr@mutms.org>
2025-12-31 10:46:35 +0100

    Revert versioning changes

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = '2025123145';
+$plugin->version = 2025123145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
