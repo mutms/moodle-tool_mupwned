d0b17a494f16eb1f96748b2d64a164a7641a00ea
Petr Skoda <petr@mutms.org>
2026-01-01 17:04:19 +0100

    Back to dev

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->version = 2025123150;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
-$plugin->release = 'mu-5.0.4-03';
+$plugin->release = 'mu-5.0.4-03+';
