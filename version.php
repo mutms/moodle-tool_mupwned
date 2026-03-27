02e8accaa52b6410a088d341efdd61ea2ac1e7da
Petr Skoda <petr@mutms.org>
2026-03-27 19:43:15 +0100

    Back to dev

--- version.php
+++ version.php
@@ -31,4 +31,4 @@ $plugin->component = 'tool_mupwned';
 $plugin->version = 2026032750;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
-$plugin->release = 'v5.0.6.04';
+$plugin->release = 'v5.0.6.04+';
