aeba04595aaa4d8b7f7623d971716564ce0f0419
Petr Skoda <petr@mutms.org>
2026-03-29 18:25:31 +0200

    Prepare release

--- version.php
+++ version.php
@@ -28,8 +28,8 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mupwned';
-$plugin->version = 2026032845;
+$plugin->version = 2026032945;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.10.05';
+$plugin->release = 'v4.5.10.06';
