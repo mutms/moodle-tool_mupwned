db1ef8f2a15cca3625ce150e71e51141e112ae42
Petr Skoda <petr@mutms.org>
2026-08-08 11:27:08 +0000

    Prepare release

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->version = 2026060545;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.12.01+';
+$plugin->release = 'v4.5.13.01';
